e6efc6a2e3cdd53d5d5a0576149405b2e1a970dc
Pujya Preyans <Pujya.Preyans@odessainc.com>
2024-02-26 18:49:48 +0530

    Added Project References

--- DinnerHub/DinnerHub.Domain/obj/Debug/net8.0/DinnerHub.Domain.AssemblyInfo.cs
+++ DinnerHub/DinnerHub.Domain/obj/Debug/net8.0/DinnerHub.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DinnerHub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95869247a73eae08a55f5e4764b865c2e307e226")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27c297da9c2589aa31766fbff965f7d46a7f13ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("DinnerHub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DinnerHub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
